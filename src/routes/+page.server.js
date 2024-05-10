d52a063cbbb1582533b0871e7106d48580b7666e
stichtingcas <daniel@climateadaptationservices.com>
2024-05-10 09:54:20 +0200

    update teksten

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,7 @@
 import { dsv } from 'd3'
 
 export async function load(){
-  const metadata = await dsv(';', "https://gist.githubusercontent.com/stichtingcas/7310a1fbb0e7b2efe841890282a2487f/raw/8f5694eeb52f576fa2c5ad567819e2fc01997b33/metadata.csv")
+  const metadata = await dsv(';', "https://gist.githubusercontent.com/stichtingcas/7310a1fbb0e7b2efe841890282a2487f/raw/97684d4270279c2cf8e482fc622afda7fddc7ca1/metadata.csv")
 
   return { metadata };
 }
