51dec11e12264ee2da3326bf3852e053803b0a0f
stichtingcas <daniel@climateadaptationservices.com>
2024-05-13 10:14:32 +0200

    Update Indicator.svelte

--- src/lib/components/Indicator.svelte
+++ src/lib/components/Indicator.svelte
@@ -98,7 +98,7 @@
       {#if $gemeenteSelection !== null}
         <Map w={wMap} h={bodyHeight*0.4} mainMapFlag={false} {color} {indicator} {getClass} />
       {/if}
-      <span style='width:100%; position:absolute; bottom:0px; display:flex; justify-content:space-between'><h5><strong>{(indicator['Link kaartverhaal'] === 'externe data') ? '' : 'Klimaateffectatlas: '} {indicator.bron}</strong></h5><h5><a target='_blank' href={indicator.link}>Meer informatie</a></h5></span>
+      <span style='width:100%; position:absolute; bottom:0px; display:flex; justify-content:space-between; pointer-events:none'><h5><strong>{(indicator['Link kaartverhaal'] === 'externe data') ? '' : 'Klimaateffectatlas: '} {indicator.bron}</strong></h5><h5 style='pointer-events:auto'><a target='_blank' href={indicator.link}>Meer informatie</a></h5></span>
     </div>
   </div>
 </div>
