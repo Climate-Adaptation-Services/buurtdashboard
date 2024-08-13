30b572bfaf18a3c98196c558289e7ae2be40e8f2
stichtingcas <daniel@climateadaptationservices.com>
2024-08-13 10:33:54 +0200

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,7 @@
 import { dsv } from 'd3'
 
 export async function load(){
-  const metadata = await dsv(';', "https://gist.githubusercontent.com/stichtingcas/7310a1fbb0e7b2efe841890282a2487f/raw/1b52d41478664345dcf14530bf7df7eac2076d5e/metadata.csv")
+  const metadata = await dsv(';', "https://raw.githubusercontent.com/Climate-Adaptation-Services/buurtdashboard-data/main/metadata.csv")
 
   return { metadata };
 }
