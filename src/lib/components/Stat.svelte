7e4001a1f92e0de877faa749116ac8b52cc5353a
stichtingcas <daniel@climateadaptationservices.com>
2024-12-05 12:50:10 +0100

    Update Stat.svelte

--- src/lib/components/Stat.svelte
+++ src/lib/components/Stat.svelte
@@ -25,8 +25,8 @@
     <rect x={170+5} y='-0.4em' fill={(indicatorValueColorscale !== null) ? indicatorValueColorscale(medianValue) : 'steelblue'} width={xScaleStats(medianValue)} height={indicatorHeight*0.45} rx="4"></rect>
     {#if regio !== 'Nederland'}
       <g class='hoveryear_{indicator.title.replaceAll(' ', '')}' transform='translate({xScaleStats(medianValueOtherYear)+175},0)' style='visibility:hidden'>
-        <line y1='-0.5em' y2={indicatorHeight*0.06} stroke='steelblue' stroke-width='5' stroke-linecap="round"/>
-        <text style='fill:steelblue; font-size:10px' text-anchor={'middle'} dx={0} dy='1.25em'>{Math.round(medianValueOtherYear*10)/10}</text>
+        <line y1='-0.5em' y2={indicatorHeight*0.06} stroke='red' stroke-width='5' stroke-linecap="round"/>
+        <text style='fill:red; font-size:10px' text-anchor={'middle'} dx={0} dy='1.25em'>{Math.round(medianValueOtherYear*10)/10}</text>
       </g>
     {/if}
     <text dx={(medianValue !== 'Geen data') ? 170 + 10 + xScaleStats(medianValue) : 180} dy='0.34em' font-size='11'>
