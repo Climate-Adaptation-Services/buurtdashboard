1682a457bd28d466ce4ee8c47fd506ee80702d55
stichtingcas <daniel@climateadaptationservices.com>
2024-12-17 11:48:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import { isoImport } from 'vite-plugin-iso-import'
 export default defineConfig({
 	plugins: [sveltekit(), isoImport()],
 	optimizeDeps: {
-    include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
+    include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep', 'd3-force']
   },
-  // build: {
-  //   rollupOptions: {
-  //     external: ['d3-force'],  // Ensure this is NOT marked as external
-  //   }
-  // }
+  build: {
+    rollupOptions: {
+      external: ['d3-force'],  // Ensure this is NOT marked as external
+    }
+  }
 });
