f484b796a7d518737d6baa431423b177c646cc90
stichtingcas <daniel@climateadaptationservices.com>
2024-12-19 11:40:31 +0100

    update links

--- src/lib/noncomponents/fetchJSONdata.js
+++ src/lib/noncomponents/fetchJSONdata.js
@@ -1,6 +1,6 @@
 export const fetchJSONdata = (async () => {
   const response = await Promise.all([
     fetch('https://raw.githubusercontent.com/Climate-Adaptation-Services/buurtdashboard-data/main/GemeenteGrenzen2023-small.json'),
-    fetch('https://buurtdashboard-data.s3.eu-north-1.amazonaws.com/Dordrecht_buurten.json')  ])
+    fetch('https://buurtdashboard-data.s3.eu-north-1.amazonaws.com/dordrecht/Dordrecht_buurten.json')  ])
   return [await response[0].json(), await response[1].json()]
 })
