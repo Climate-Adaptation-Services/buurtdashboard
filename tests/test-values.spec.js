124940a512035218807e6fa86bce05b0a7c160d0
stichtingcas <daniel@climateadaptationservices.com>
2025-01-07 16:50:11 +0100

    update

---
+++ tests/test-values.spec.js
@@ -0,0 +1,9 @@
+// // @ts-check
+// import { test } from '@playwright/test';
+
+// ['-dev', ''].forEach(locationName => {    
+//   test(`${locationName}test`, async ({ page }) => {
+//     await page.goto(`https://buurtdashboard${locationName}.vercel.app`);
+    
+//   });
+// })
