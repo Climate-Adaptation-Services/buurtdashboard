b8405da728850f4604918e26e4043ff00cef2b61
stichtingcas <daniel@climateadaptationservices.com>
2025-06-12 13:10:19 +0200

    bufix

--- src/lib/noncomponents/getMostCommonClass.js
+++ src/lib/noncomponents/getMostCommonClass.js
@@ -2,10 +2,11 @@ import { getIndicatorAttribute } from "./getIndicatorAttribute";
 
 // deze functie is nodig om de kleur te bepalen van een neighbourhood in de aggregatedIndicator kaart
 export function getMostCommonClass(indicator, feature) {
+
   let mostCommon = ''
   let highestValue = 0
   Object.keys(indicator.classes).forEach(key => {
-    if (feature.properties[getIndicatorAttribute(indicator, indicator.classes[key])] > highestValue) {
+    if (+feature.properties[getIndicatorAttribute(indicator, indicator.classes[key])] > highestValue) {
       highestValue = feature.properties[getIndicatorAttribute(indicator, indicator.classes[key])]
       mostCommon = key
     }
