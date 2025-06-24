609c348cba0efb5642f0e1ee651a923a6446d940
stichtingcas <daniel@climateadaptationservices.com>
2025-06-24 14:42:54 +0200

    remove vercel

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DhACiXYB.js","_app/immutable/chunks/BtgGQ92H.js","_app/immutable/chunks/YORhUFDs.js","_app/immutable/chunks/DA1yglS5.js","_app/immutable/chunks/B5Lywc65.js"];
-export const stylesheets = [];
-export const fonts = [];
