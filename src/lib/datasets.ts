a759863f2e54ce9be47c9c2924f510c7c43dd540
stichtingcas <daniel@climateadaptationservices.com>
2025-10-09 09:59:34 +0200

    Update datasets.ts

--- src/lib/datasets.ts
+++ src/lib/datasets.ts
@@ -18,7 +18,7 @@ export const DEFAULT_METADATA_ENGLISH_URL =
   "https://buurtdashboard-data.s3.eu-north-1.amazonaws.com/buurtdashboard-KEA/metadata/EN-buurtdashboard-metadata-050825-01.csv";
 
 export const DEFAULT_CSV_DATA_URL =
-  "https://buurtdashboard-data.s3.eu-north-1.amazonaws.com/buurtdashboard-KEA/csvdata/basiskaartGroen071025(b10Buurt2024BasiskaartGroen).csv.gz";
+  "https://buurtdashboard-data.s3.eu-north-1.amazonaws.com/buurtdashboard-KEA/csvdata/basiskaartGroen081025(b10Buurt2024BasiskaartGroen).csv.gz";
 
 // Dordrecht config URLs
 export const DORDRECHT_METADATA_URL =
